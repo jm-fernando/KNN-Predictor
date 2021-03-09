e6c2f6f5aaef248af0bc2d8e0f47a64fb97345a7
Jerome Fernando <jmfernando@dons.usfca.edu>
2021-03-09 10:50:54 -0800

    Adding Pokemon.java into my folder

---
+++ Pokemon_Jerome/Pokemon.java
@@ -0,0 +1,75 @@
+//*created by Jenica*//
+//*edited by Jerome*//
+package abstraction;
+
+public abstract class Pokemon {
+	private String name;
+	private String color;
+	private boolean hasTrainer;
+	private boolean canMove;
+	
+	//Constructor
+	public Pokemon(String nParam, String cParam) {
+		this.name = nParam;
+		this.color = cParam;
+		this.hasTrainer = false;
+	}
+	
+	public Pokemon(String nParam, String cParam, boolean hParam) {
+		this.name = nParam;
+		this.color = cParam;
+		this.hasTrainer = hParam;
+	}
+
+	public void squirtleMove() {
+		  this.canMove = true;
+	  }
+	  
+	  public void pikachuMove() {
+		  this.canMove = true;
+	  }
+	//Default Constructor
+	public Pokemon() {
+		this.name = "";
+		this.color = "";
+		this.hasTrainer = false;
+	}
+	
+	//Accessor
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public boolean hasTrainer() {
+		return this.hasTrainer();
+	}
+	
+	//Mutator
+	public void setName(String nParam) {
+		this.name = nParam;
+	}
+	
+	public void setColor(String cParam) {
+		this.color = cParam;
+	}
+	
+	public void setHasTrainer(boolean hParam) {
+		this.hasTrainer = hParam;
+	}
+		
+	//Method
+	public String toString() {
+		return "I am a Pokemon: " + this.name + "Color: " + this.color + "Trainer: " + this.hasTrainer;
+	}
+	
+	//Abstract Method
+	public abstract String speak();
+	public abstract String trainerName();
+
+		
+}
+		
